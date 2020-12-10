ae6c5e48a5b3c51d349c18f20344932aaa4c795e
c501306014 <c501306014@gmail.com>
2020-12-10 14:17:44 +0900

    add footer.php header.php フッターとヘッダーを分割

--- index.php
+++ index.php
@@ -1,32 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="utf-8">
-    <title>Portfolio</title>
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-    <link rel="stylesheet" href="./css/stylesheet.css">
-    <script src="https://kit.fontawesome.com/3a7917df6a.js" crossorigin="anonymous"></script>
-    <?php wp_head(); ?>
-</head>
-
-<body>
-    <!----- header ----->
-    <nav>
-        <div class="container">
-            <a href="#" class="logo">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="logo">
-            </a>
-            <div class="menu">
-                <a href="#">Top</a>
-                <a href="#">About</a>
-                <a href="#">Works</a>
-                <a href="#">Contact</a>
-            </div>
-        </div>
-    </nav>
+<?php get_header(); ?>
 
     <!-- Top -->
     <section id="top-wrapper">
@@ -131,7 +103,5 @@
             </form>
         </div>
     </section>
-    <?php wp_footer(); ?>
-</body>
 
-</html>
+<?php get_footer(); ?>
