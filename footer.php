fe7636fc321e6afc2a7e720435fbc0d0f17e656a
c501306014 <c501306014@gmail.com>
2020-12-16 12:07:26 +0900

    refactor footer.php トップへ戻るボタンのアニメーションをvueで表現した。

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 
-
-<div id="scroll-to-top" v-bind:class="{'fade-in': isActive, 'fade-out': !isActive}">
-    <a href="#" v-scroll-to="'#top-wrapper'" title="topへ戻る">
-        <i class="fas fa-arrow-up"></i>
-    </a>
+<!-- topへ戻るボタン -->
+<div id="scroll-to-top">
+    <transition name="fade">
+        <a v-if="isActive" href="#" v-scroll-to="'#top-wrapper'" title="topへ戻る">
+            <i class="fas fa-arrow-up"></i>
+        </a>
+    </transition>
 </div>
-
-
 
 </div>
     <?php wp_footer(); ?>
