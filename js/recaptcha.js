a7f17670e90ea86befe725cb91dc5973e2d8eba0
c501306014 <c501306014@gmail.com>
2021-02-25 11:49:06 +0900

    表示位置を変更するのではなく、Contactまでスクロールしたらフェードインさせるように変更

--- js/recaptcha.js
+++ js/recaptcha.js
@@ -1,11 +1,25 @@
-/* reCAPTCHA3のUI表示位置をCONTACT送信ボタンの左に配置 */
-jQuery(window).on('load', function(){
-    let submitBtn_offset = jQuery('.submit-btn').offset();
-    let grecaptchaBadge = jQuery('.grecaptcha-badge');
+/* reCAPTCHA3のUIをCONTACT表示位置にきたらフェードインさせる */
+jQuery(function(){
+    let is_show = false;
 
-    grecaptchaBadge.css('position', 'absolute')
-    grecaptchaBadge.offset({
-        top: submitBtn_offset.top,
-        left: submitBtn_offset.left,
+
+    jQuery(window).on('load', function(){
+        jQuery('.grecaptcha-badge').css('display', 'none')
+    })    
+
+    jQuery(window).on('scroll  resize', function(){
+        let top = jQuery('#contact-wrapper').offset().top;
+
+        if ( top - 200 < jQuery(this).scrollTop()){
+            if( is_show === false ){
+                jQuery('.grecaptcha-badge').hide().fadeIn();
+                is_show = true;
+            }
+        }else{
+            if( is_show === true){
+                jQuery('.grecaptcha-badge').fadeOut();
+                is_show = false;
+            }
+        }
     })
 })
